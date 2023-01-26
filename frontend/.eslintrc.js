09908edca2b3b5c8bea168439f65bfb45d31f910
tavakai <tavakay95@mail.ru>
2023-01-26 15:21:48 +0300

    init project

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'no-console': 0,
+    'no-restricted-exports': 0,
+    'default-param-last': 0,
+    'import/prefer-default-export': 0,
+    'react/prop-types': 0,
+    'import/no-extraneous-dependencies': 0,
+    'react/react-in-jsx-scope': 0,
+  },
+};
